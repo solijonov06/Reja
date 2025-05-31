155b403884be464899f780923f8b5f82e7a00458
Tony <diyorbeksolijonov27@gmail.com>
2025-05-31 14:18:25 +0900

    feat: build traditional frontend with EJS framework

--- server.js
+++ server.js
@@ -10,20 +10,32 @@ app.use(express.urlencoded({ extended: true }));
 
 //2: Session
 //3:Views code
-app.set('views', 'views');
+app.set('views', 'views2');
 app.set('view engine', 'ejs');
 
 //4: Routing code
-app.get('/hello', function (req, res){
-    res.end(`<h1 style = "background: green">Hello World</h1>`);
+// app.get('/hello', function (req, res){
+
+//     res.end(`<h1 style = "background: green">Hello World</h1>`);
+// });
+
+// app.get('/gifts', function (req, res) {
+//         res.end(`<h1 style = "background: yellow">You are onpage of gifts.</h1>`);
+//     });
+app.post("/create-item", function (req, res) {
+    console.log(req);
+    res.json({test:"succes"});
 });
 
-app.get('/gifts', function (req, res) {
-        res.end(`<h1 style = "background: yellow">You are onpage of gifts.</h1>`);
-    });
+app.get("/", function (req,res) {
+      res.render("harid2");
+});
+
+
 
 const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, () => {
+let PORT = 3001;
+server.listen(PORT, function () {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+//5: Error handling code
