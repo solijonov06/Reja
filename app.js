365b18d1169ecaac3fdb0b4e9b4f18441183d99f
Tony <diyorbeksolijonov27@gmail.com>
2025-06-08 19:13:32 +0900

    feat: CRUD operations

---
+++ app.js
@@ -0,0 +1,72 @@
+console.log("Web server started");
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+//Call MongoDB
+const db = require("./server.js");
+
+let user;
+fs.readFile("database/user.json", "utf-8", function (err, data) {
+    if (err) {
+        console.log("Error reading user data:", err);
+    } else {
+        user = JSON.parse(data);
+        console.log("User data loaded successfully:", user);
+    }
+});
+
+//1: Kirish code
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2: Session
+//3:Views code
+app.set('views', 'views2');
+app.set('view engine', 'ejs');
+
+//4: Routing code
+// app.get('/hello', function (req, res){
+
+//     res.end(`<h1 style = "background: green">Hello World</h1>`);
+// });
+
+// app.get('/gifts', function (req, res) {
+//         res.end(`<h1 style = "background: yellow">You are onpage of gifts.</h1>`);
+//     });
+app.post("/create-item", function (req, res) {
+     console.log("user entered /create-item");
+    console.log(req.body);
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja:new_reja},(err,data)=>{
+        if (err){
+            console.log(err);
+            res.end("Something went wrong");
+        }else{
+            res.end("New plan created successfully");
+        }
+    });
+});
+
+app.get("/", function (req,res) {
+    console.log("user entered /")
+    db.collection("plans").find().toArray((err,data)=>{
+        if(err){
+            console.log(err);
+            res.end("something went wrong")
+        }else{
+            console.log(data);
+            res.render("reja", {items: data});
+        }
+    });
+          
+});
+
+app.get("/author", function (req,res) {
+      res.render("author", {user: user});
+});
+
+
+
+module.exports= app;
--- server.js
+++ server.js
@@ -1,56 +1,30 @@
-console.log("Web server started");
-const express = require('express');
-const app = express();
 const http = require('http');
-const fs = require("fs");
+const mongodb = require('mongodb')
 
-let user;
-fs.readFile("database/user.json", "utf-8", function (err, data) {
-    if (err) {
-        console.log("Error reading user data:", err);
-    } else {
-        user = JSON.parse(data);
-        console.log("User data loaded successfully:", user);
-    }
-});
+let db;
+const connectionString =
 
-//1: Kirish code
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+"mongodb+srv://Tony:Mrsolijonov_06@cluster0.gs9zifq.mongodb.net/";
 
-//2: Session
-//3:Views code
-app.set('views', 'views2');
-app.set('view engine', 'ejs');
+mongodb.MongoClient.connect(
+    connectionString, 
+    {
+         useNewUrlParser: true, 
+        useUnifiedTopology: true
+    },
+        (err, client)=>{
+            if (err) console.log("ERROR on connection to MongoDB");
+            else{
+                console.log("Connected to MongoDB");
+                const db = client.db("Reja"); // <-- use your actual DB name here
+                module.exports = db;
 
-//4: Routing code
-// app.get('/hello', function (req, res){
+                const app = require('./app.js');
+                const server = http.createServer(app);
+                let PORT = 3000;
+                server.listen(PORT, function () {
+                console.log(`Server is running on http://localhost:${PORT} `);
+                });
+            }
+        });
 
-//     res.end(`<h1 style = "background: green">Hello World</h1>`);
-// });
-
-// app.get('/gifts', function (req, res) {
-//         res.end(`<h1 style = "background: yellow">You are onpage of gifts.</h1>`);
-//     });
-app.post("/create-item", function (req, res) {
-    console.log(req);
-    res.json({test:"succes"});
-});
-
-app.get("/", function (req,res) {
-      res.render("reja");
-});
-
-app.get("/author", function (req,res) {
-      res.render("author", {user: user});
-});
-
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`Server is running on http://localhost:${PORT} `);
-});
-//5: Error handling code
